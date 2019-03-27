12987a7fd77c7425effb565ab302c61d5333f86c
R. Kyle Bocinsky <bocinsky@gmail.com>
2019-03-27 15:42:00 -0600

    remove icon

--- src/mesonet/test.py
+++ src/mesonet/test.py
@@ -9,10 +9,14 @@ environment you are running this script in.
 """
 
 from mesonet.connect import connect
+from zentra.api import *
+# import mesonet.zentra
+import src.mesonet.zentra
 import pandas as pd
 from dfply import *
 from os import getenv
-from zentra.api import *
+from pandas import Timestamp
+
 from datetime import datetime, timedelta
 
 con = connect(username=getenv("cfcsql_un"),
@@ -84,3 +88,37 @@ test = list(
 # all_readings = ZentraReadings(station="06-00761",
 #                           token=token,
 #                               start_mrid=1)
+
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', 500)
+
+test = src.mesonet.zentra.ZentraReadings(json_file="./tests/data/Readings2019.03.25.json")
+
+test_out = test.prepare_raw()
+
+# Delete all rows
+con.execute("DELETE FROM observations.raw")
+
+
+
+test_out.replace('NA', np.nan).to_sql(con=con,
+                                      name="raw",
+                                      schema="observations",
+                                      if_exists='append',
+                                      chunksize=np.int(np.floor((2000 / len(test_out.columns)))),
+                                      index=False,
+                                      method='multi')
+
+pd.read_sql_table(con=con,
+                                      table_name="raw",
+                                      schema="observations")
+
+
+test_out.
+
+test_out.value
+
+src.mesonet.zentra.get_readings_since_last_mrid(station_key="arskeogh",
+                                                token=token,
+                                                mesonet_db_con=con,
+                                                start_mrid=30607).prepare_raw()
--- src/mesonet/zentra.py
+++ src/mesonet/zentra.py
@@ -2,20 +2,25 @@
 
 """
 import zentra.api as zentra
+import pandas as pd
 
 
 def get_readings_since_last_mrid(station_key, token, mesonet_db_con, start_mrid=None):
     """
-    Gets a user token using a POST request to the Zentra API.
-    Wraps build and parse functions.
+    Gets a the Zentra readings for a Mesonet station since a given
+    mrid, or else since the last downloaded mrid for that station in
+    the Mesonet database.
 
     Parameters
     ----------
-    username : str
-        The username
-    password : str
-        The password
-
+    station_key : str
+        The unique identifier for a station
+    token : ZentraToken
+        A valid ZentraToken object
+    mesonet_db_con : Engine
+        A sqlalchemy Engine object, as created by mesonet.connect.connect()
+    start_mrid : int, optional
+        Return readings with mrid ≥ start_mrid.
     """
 
     logger_sn = pd.read_sql(sql=("SELECT logger_sn " +
@@ -37,7 +42,39 @@ def get_readings_since_last_mrid(station_key, token, mesonet_db_con, start_mrid=
 
 class ZentraReadings(zentra.ZentraReadings):
     def prepare_raw(self):
-        return self.timeseries[0].values
+        out = [x
+                   .values
+                   .rename({'description': 'measurement'},
+                           axis='columns')
+                   .assign(timeseries=i,
+                           logger_sn=self.device_info['device_sn'])[['logger_sn',
+                                                                     'timeseries',
+                                                                     'mrid',
+                                                                     'datetime',
+                                                                     'port',
+                                                                     'measurement',
+                                                                     'units',
+                                                                     'value']]
+               for i, x
+               in enumerate(self.timeseries)]
+        out = pd.concat(out)
+
+        return (out
+                .assign(timeseries=out.timeseries.astype('uint8'),
+                        datetime=out.datetime.dt.tz_localize("America/Denver"),
+                        port=out.port.astype('uint8'),
+                        mrid=out.mrid.astype('uint32'),
+                        units=out.units.str.strip(),
+                        measurement=out.measurement.str.strip(),
+                        value=out.value.round(5))
+                .drop_duplicates()
+                .dropna()
+                .sort_values(['logger_sn',
+                              'timeseries',
+                              'mrid',
+                              'datetime',
+                              'port',
+                              'measurement']))
 
     def prepare_level_0(self):
         return self.prepare_raw()
@@ -50,13 +87,3 @@ class ZentraReadings(zentra.ZentraReadings):
 
     def prepare(self):
         return self.prepare_raw()
-
-
-
-ZentraReadings(json_file = "/Volumes/Resources$/Data/Mesonet/ZentraTest/API-Output/ClimateOffice/Readings/arskeogh/2019/Readings2019.03.25.json").prepare()
-
-
-get_readings_since_last_mrid(station_key="arskeogh",
-                             token=token,
-                             mesonet_db_con=con,
-                             start_mrid=30607).prepare()
