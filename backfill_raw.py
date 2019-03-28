6c74bf18b9e92c938b83cff14e79fd11a340e038
R. Kyle Bocinsky <bocinsky@gmail.com>
2019-03-27 22:26:21 -0600

    coercing to float

--- backfill_raw.py
+++ backfill_raw.py
@@ -21,21 +21,21 @@ con.execute("DELETE FROM " + schema + "." + table).close()
                 append=True
                 )
     for x
-    # in list(
-    #     Path('//mcofiles.cfc.umt.edu/Resources$/Data/Mesonet/ZentraTest/API-Output/ClimateOffice/Readings').glob(
-    #         '**/*.json'))
     in list(
-        Path('./tests/data/').glob(
+        Path('//mcofiles.cfc.umt.edu/Resources$/Data/Mesonet/ZentraTest/API-Output/ClimateOffice/Readings').glob(
             '**/*.json'))
+    # in list(
+    #     Path('./tests/data/').glob(
+    #         '**/*.json'))
 ]
 
-test = ZentraReadings(json_file=Path("./tests/data/Readings2017.06.26.json")).timeseries[0].values
-test \
-    .assign(value=test.value.astype('float').round(5))
-
-write_to_db(ZentraReadings(json_file=Path("./tests/data/Readings2017.06.26.json")).prepare_raw(),
-            con=con,
-            schema="observations",
-            table="raw",
-            append=True
-            )
+# test = ZentraReadings(json_file=Path("./tests/data/Readings2017.06.26.json")).timeseries[0].values
+# test \
+#     .assign(value=test.value.astype('float').round(5))
+#
+# write_to_db(ZentraReadings(json_file=Path("./tests/data/Readings2017.06.26.json")).prepare_raw(),
+#             con=con,
+#             schema="observations",
+#             table="raw",
+#             append=True
+#             )
