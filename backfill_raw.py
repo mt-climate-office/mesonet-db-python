8f34a621cc93b877a13d91dfd2c945a7928d6358
R. Kyle Bocinsky <bocinsky@gmail.com>
2019-03-29 14:17:15 -0600

    trying and ccatching

--- backfill_raw.py
+++ backfill_raw.py
@@ -3,6 +3,7 @@ from mesonet.connect import connect
 from mesonet.zentra import ZentraReadings
 from mesonet.write_to_db import write_to_db
 from pathlib import Path
+import warnings
 
 # import multiprocessing
 #
@@ -19,6 +20,13 @@ table = 'raw'
 
 # Start fresh
 con.execute("DELETE FROM " + schema + "." + table).close()
+
+
+def catch(func, handle=lambda e: e, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        return handle(e)
 
 
 def write(x):
@@ -38,16 +46,15 @@ def write(x):
 #             '**/*.json')))
 # pool.close()
 
-[
-    write(x)
-    for x
-    in list(
-    Path('//mcofiles.cfc.umt.edu/Resources$/Data/Mesonet/ZentraTest/API-Output/ClimateOffice/Readings').glob(
-        '**/*.json'))
-    # in list(
-    #     Path('./tests/data/').glob(
-    #         '**/*.json'))
-]
+files = Path('//mcofiles.cfc.umt.edu/Resources$/Data/Mesonet/ZentraTest/API-Output/ClimateOffice/Readings').glob(
+    '**/*.json')
+
+for file in files:
+    try:
+        write(x)
+    except:
+        warnings.warn("Something went wrong! Skipping this file.", RuntimeWarning)
+        pass
 
 #
 # write_to_db(ZentraReadings(json_file=Path("./tests/data/Readings2018.06.17.json")).prepare_raw(),
