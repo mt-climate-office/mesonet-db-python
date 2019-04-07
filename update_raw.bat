6598607cf8bae5c20d4af9ac93462e53c5766edc
R. Kyle Bocinsky <bocinsky@gmail.com>
2019-04-07 14:23:49 -0600

    added batch file

--- update_raw.bat
+++ update_raw.bat
@@ -1,4 +1,4 @@
 @echo on
-conda activate mesonet
-python C:\MCO\Mesonet\py\Mesonet-DB\update_raw.py
-conda deactivate
+call conda activate mesonet
+call python C:\MCO\Mesonet\py\Mesonet-DB\update_raw.py
+call conda deactivate
